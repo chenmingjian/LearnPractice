0d2c87ffcbeba172bc90cfae3bc0658033865bdc
chen <chen@chenmingjian.com>
2018-03-05 22:05:37 +0800

    .

--- ML_LYH/HW3/famble_2.py
+++ ML_LYH/HW3/famble_2.py
@@ -12,18 +12,18 @@ img_rows, img_cols = 28, 28
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_testl.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_testl.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print('x_train_shape:', x_train_shape)
+print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
